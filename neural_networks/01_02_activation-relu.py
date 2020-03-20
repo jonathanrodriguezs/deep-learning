98a0625b0a9431f87bb4c37f5699627913d82f34
Jonathan Rodriguez Sanchez <jonathan.rodriguezs@softtek.com>
2020-03-20 02:08:09 -0600

    chore: add some comments

--- neural_networks/01_02_activation-relu.py
+++ neural_networks/01_02_activation-relu.py
@@ -2,30 +2,29 @@ import numpy as np
 
 
 def relu(input):
-    return max(input, 0)
-
+  return max(input, 0)
 
 def calc_node_output(row_data, weights):
-    node_input = (row_data * weights).sum()
-    node_output = relu(node_input)
-    return node_output
+  node_input = (row_data * weights).sum()
+  node_output = relu(node_input)
+  return node_output
 
 
 if __name__ == "__main__":
-    # Hidden layer nodes weights
-    weights = {
-        'node_0': np.array([1, 1]),
-        'node_1': np.array([-1, 1]),
-        'output': np.array([2, -1])
-    }
+  # Hidden layer nodes weights
+  weights = {
+      'node_0': np.array([1, 1]),
+      'node_1': np.array([-1, 1]),
+      'output': np.array([2, -1])
+  }
 
-    # Input
-    input_data = np.array([2, 3])
-    # Hidden layer noddes
-    node_0 = calc_node_output(input_data, weights['node_0'])
-    node_1 = calc_node_output(input_data, weights['node_1'])
-    hidden_layer = np.array([node_0, node_1])
-    # Output
-    model_output = calc_node_output(hidden_layer, weights['output'])
+  # Input
+  input_data = np.array([2, 3])
+  # Hidden layer noddes
+  node_0 = calc_node_output(input_data, weights['node_0'])
+  node_1 = calc_node_output(input_data, weights['node_1'])
+  hidden_layer = np.array([node_0, node_1])
+  # Output
+  model_output = calc_node_output(hidden_layer, weights['output'])
 
-    print(model_output)
+  print(model_output)
--- neural_networks/02_01_gradient-descent.py
+++ neural_networks/02_01_gradient-descent.py
@@ -4,42 +4,44 @@ from sklearn.metrics import mean_squared_error
 
 
 def get_prediction(input_data, weights):
-    return (weights * input_data).sum()
-
+  return (weights * input_data).sum()
 
 def get_slope(pred, target):
-    slope = 2 * input_data * (pred - target)
-    return slope
-
+  slope = 2 * input_data * (pred - target)
+  return slope
 
 def get_mse(pred, target):
-    mse = mean_squared_error(target, pred)
-    return mse
+  mse = mean_squared_error(target, pred)
+  return mse
 
 
 if __name__ == "__main__":
-    target = 2
-    learning_rate = 0.01
-    max_iterations = 100
-    acceptation = 0.001
+  # Training configuration
+  learning_rate = 0.01
+  max_iterations = 100
+  acceptation = 0.001
+  target = 2
 
-    input_data = np.array([5, 2, 1])
-    weights = np.array([3, 0, -5])
-    mse_list = []
+  # Neural network
+  input_data = np.array([5, 2, 1])
+  weights = np.array([3, 0, -5])
+  mse_list = []
 
-    for i in range(max_iterations):
-        pred = get_prediction(input_data, weights)
-        slope = get_slope(pred, target)
-        mse = get_mse([pred], [target])
+  for i in range(max_iterations):
+    # Calculate slop and mse based on the prediction
+    pred = get_prediction(input_data, weights)
+    slope = get_slope(pred, target)
+    mse = get_mse([pred], [target])
 
-        mse_list.append(mse)
-        weights = weights - learning_rate * slope
-        accuracy = abs(target - pred)
-        if (accuracy < acceptation):
-            print("Weights ", weights)
-            break
+    weights = weights - learning_rate * slope
+    mse_list.append(mse)
 
-    plt.plot(mse_list)
-    plt.ylabel('Mean squared error')
-    plt.xlabel('Iterations')
-    plt.show()
+    if (abs(target - pred) < acceptation):
+      print("Weights ", weights)
+      break
+  
+  # Plot the Mean squared error variation
+  plt.plot(mse_list)
+  plt.ylabel('Mean squared error')
+  plt.xlabel('Iterations')
+  plt.show()
